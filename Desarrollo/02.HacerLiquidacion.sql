f7ab14cd897fc8aa912079b3599fcda6a375a8d3
sebastian <sebastiand.fournier@gmail.com>
2021-04-19 19:03:39 -0300

    store procedures, corregidos

--- Desarrollo/02.HacerLiquidacion.sql
+++ Desarrollo/02.HacerLiquidacion.sql
@@ -1,3 +1,14 @@
 --procedure que hace la liquidacion de las horas segun proyecto y rol
 
-delimiter $$ CREATE PROCEDURE hacerliquidacion()  begin  INSERT INTO liquidacion (rol, proyecto, cliente, cantidad_horas, mes_ano,fechaliquidacion) SELECT rol,proyecto, id_cliente, SUM(cantidad_horas), fecha, curdate() FROM asignacion INNER JOIN participacion on asignacion.id_participacion=participacion.id_participacion INNER JOIN proyecto on participacion.proyecto=proyecto.proyecto_id GROUP BY rol, proyecto, MONTH(fecha) ORDER BY rol, proyecto;    END; $$
+delimiter $$
+CREATE PROCEDURE hacerliquidacion()
+ begin
+
+INSERT INTO liquidacion (rol, proyecto, cliente, cantidad_horas, fecha,fecha_liqudiacion) SELECT rol,proyecto, id_cliente, SUM(cantidad_horas), fecha, curdate() FROM asignacion INNER JOIN participacion on asignacion.id_participacion=participacion.id_participacion INNER JOIN proyecto on participacion.proyecto=proyecto.proyecto_id 
+GROUP BY rol, proyecto, MONTH(fecha)
+ORDER BY rol, proyecto;
+
+
+
+END;
+$$
