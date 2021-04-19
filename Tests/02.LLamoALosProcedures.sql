8177a349501a95664bfbea5c899f46e66700112d
sebastian <sebastiand.fournier@gmail.com>
2021-04-19 18:49:23 -0300

    Llamo a los store procedures

---
+++ Tests/02.LLamoALosProcedures.sql
@@ -0,0 +1,6 @@
+
+
+--Orden de entrada de datos: (legajo, proyecto, horas,fecha)   'año-mes-dia'   
+
+
+CALL RendicionDeHoras(2, 3, 15,'2020-07-00'); CALL RendicionDeHoras(1, 2, 2,'2020-07-02'); CALL RendicionDeHoras(3, 6, 3,'2020-07-06'); CALL RendicionDeHoras(5, 3, 34,'2020-04-00'); CALL RendicionDeHoras(4, 2, 5,'2020-06-13'); CALL RendicionDeHoras(2, 2, 4,'2020-04-19'); CALL RendicionDeHoras(3, 3, 15,'2020-03-00'); CALL RendicionDeHoras(6, 1, 34,'2020-09-00'); CALL RendicionDeHoras(7, 4, 56,'2020-12-00'); CALL RendicionDeHoras(8, 3, 34,'2020-01-00'); CALL RendicionDeHoras(6, 3, 4,'2020-03-24'); CALL RendicionDeHoras(7, 2, 1,'2020-03-09'); CALL RendicionDeHoras(8, 1, 2,'2020-09-09'); CALL RendicionDeHoras(8, 2, 1,'2020-03-01');        CALL hacerliquidacion;
